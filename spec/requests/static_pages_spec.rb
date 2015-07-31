f42c6f58e965420096ad2aa94f24707839a3ee47
yokogi <yokogi@e-reverse.com>
2015-07-31 19:32:53 +0900

    rspec change

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,4 +56,22 @@ describe "StaticPages" do
 
   end
 
+  describe "Contact page" do
+
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      #get static_pages_index_path
+      #response.status.should be(200)
+
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{title}")
+    end
+
+  end
+
 end
